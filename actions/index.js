024b6127ad12a10666106454497da33b8d95df69
Anonymous <anonymous@example.com>
2021-01-14 21:30:07 +0100

    Fixed #diy text

--- actions/index.js
+++ actions/index.js
@@ -3,7 +3,7 @@ const { render } = require('../lib/render');
 module.exports = function (req, res) {
     let usp = [
         { channel: '#jobs', description: 'Top paid jobs in Berlin, Munich, Hamburg and other cities in Germany.' },
-        { channel: '#auto', description: 'Howtos for Driver\'s license, carsharing tips and car maintenance.' },
+        { channel: '#auto', description: 'Howtos for Driver\'s license, carsharing and car ownership tips.' },
         { channel: '#dev', description: 'Leet code discussions, test tasks help, frameworks and language experience exchange.' },
         { channel: '#diy', description: 'Hobby projects, 3d-printing, renovations, repairs and car/bike maintenance.' },
     ];
