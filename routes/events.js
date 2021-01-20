c710af202ce056c302c0a754f73e89ff689ab98c
Aleksandr Bogdanov <syn@syn.im>
2021-01-20 16:37:18 +0100

    WIP

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,11 @@
-const express = require("express");
+const express = require("express")
+const bodyParser = require("body-parser")
 const { createEventAdapter } = require('@slack/events-api')
 const config = require("../config")
 const {slack} = require("../lib/slack")
 const {sync} = require('../lib/verification')
 
+const bodyUrlencoded = bodyParser.urlencoded({ extended: false })
 const banMap = sync.syncMaps(config.twilioSyncBanMapId)
 
 const slackEventAdapter = createEventAdapter(config.slackSigningSecret, {includeBody: true});
@@ -23,8 +25,37 @@ async function ban(channelId, userId, expiration){
   await banMap.create({key: banKey(channelId, userId), data: {expiration}})
 }
 
+async function unban(channelId, userId){
+  await banMap.syncMapItems(banKey(channelId, userId)).remove()
+}
+
+async function isModerator(userId, channelId){
+  return false
+}
+
+async function banCommand(req, res){
+  const respond = t => res.status(200).send({"response_type": "ephemeral", "text": t})
+
+  const {text, channel_id, user_id} = req.body // <@U1234|user> in text requires escaped command text
+
+  if(!(await isModerator(user_id, channel_id))){
+    return respond('you cannot do that')
+  }
+
+  for(const user of text.matchAll(/<@(U[\d\w]+)>/i)){
+    if(text.indexOf('remove') === -1) {
+      await ban(user[1], channel_id)
+    }else{
+      await unban(user[1], channel_id)
+    }
+  }
+
+  respond('done')
+}
+
 slackEventAdapter.on('member_joined_channel', async e => {
   const ts = parseInt(e.event_ts.replace('.', ''))
+
   if(await isBanned(e.user, e.channel, ts)){
     try {
       await slack.conversations.kick({channel: e.channel, user: e.user})
@@ -35,5 +66,6 @@ slackEventAdapter.on('member_joined_channel', async e => {
 })
 
 const router = express.Router()
-router.use(slackEventAdapter.expressMiddleware())
+router.use('/', slackEventAdapter.expressMiddleware())
+router.post('/ban', bodyUrlencoded, banCommand)
 module.exports = router
