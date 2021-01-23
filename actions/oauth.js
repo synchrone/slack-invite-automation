a2a123f98ff4bc47f850bcdaef0332f4fe7d56f5
Aleksandr Bogdanov <syn@syn.im>
2021-01-23 16:21:08 +0100

    definitely fixing oauth url handlers

--- actions/oauth.js
+++ actions/oauth.js
@@ -6,22 +6,23 @@ const slack = new WebClient()
  * OAuth is required for legacy-mode access on admin user's behalf, since users.admin.invite method is not accessible in OAuth2
  */
 function oauthActionFactory(path, extra) {
+    const redirectUrl = `${config.inviterUrl}/${path}`
     return async function oauthHandler(req, res) {
-    if (!req.query.code) {
-            res.redirect(`https://${config.slackUrl}/oauth?client_id=${config.slackClientId}&redirect_uri=${config.inviterUrl}/${path}&${extra}`)
-    } else {
-        try {
-            const response = await slack.oauth.access({
-                code: req.query.code,
-                client_id: config.slackClientId,
-                client_secret: config.slackClientSecret
-            })
-            res.status(200).send(response)
-        }catch(e){
-            console.log(e)
-            res.status(500).send(e.toString())
-        }
-
+        if (!req.query.code) {
+            res.redirect(`https://${config.slackUrl}/oauth?client_id=${config.slackClientId}&redirect_uri=${redirectUrl}&${extra}`)
+        } else {
+            try {
+                const response = await slack.oauth.access({
+                    code: req.query.code,
+                    client_id: config.slackClientId,
+                    client_secret: config.slackClientSecret,
+                    redirect_uri: redirectUrl
+                })
+                res.status(200).send(response)
+            }catch(e){
+                console.log(e)
+                res.status(500).send(e.toString())
+            }
         }
     }
 }
